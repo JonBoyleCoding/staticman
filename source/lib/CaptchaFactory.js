1d4d75366f56780fefdbe08fe368a87cbcc6a90f
JonBoyleCoding <programmerfalcon@gmail.com>
2023-02-06 19:34:42 +0000

    Fixes for captcha factory

--- source/lib/CaptchaFactory.js
+++ source/lib/CaptchaFactory.js
@@ -1,19 +1,19 @@
-import errorHandler from './ErrorHandler';
-import HCaptcha from './HCaptcha';
-import ReCaptcha from './ReCaptcha';
+import errorHandler from './ErrorHandler'
+import HCaptcha from './HCaptcha'
+import ReCaptcha from './ReCaptcha'
 
 export default (service, options) => {
   let captcha
   switch (service) {
     case 'HCaptcha':
-      captcha = new HCaptcha(options);
+      captcha = new HCaptcha(options)
       break;
     case 'ReCaptcha':
-      captcha = new ReCaptcha(options);
+      captcha = new ReCaptcha(options)
       break;
     default:
       throw errorHandler('CAPTCHA_SERVICE_MISSING')
   }
-  captcha.verifyConfig();
+  captcha.verifyConfig()
   return captcha
-};
+}
